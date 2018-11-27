2b335b6fe75f0755eca02352cadb0c25ad4cc4f3
Alfredo Mendoza <almendoza25@gmail.com>
2018-11-27 10:10:07 -0800

    Checkpoint 2 completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+var config = {
+    apiKey: "AIzaSyAKAlASuiNZl6tTcEMvlW718o_rHwtQ3dM",
+    authDomain: "bloc-chat-react-438d8.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-438d8.firebaseio.com",
+    projectId: "bloc-chat-react-438d8",
+    storageBucket: "bloc-chat-react-438d8.appspot.com",
+    messagingSenderId: "426496830872"
+  };
+  firebase.initializeApp(config);
+
+  const App = () => (
+     <main className="wrapper">
+       <header className="app-title">
+         <h1>Bloc Chat</h1>
+       </header>
+       <section className="rows">
+         <section className="room-list">
+           <RoomList firebase={firebase} />
+         </section>
+         <section className="message-list">
+           <p>Placeholder for Message List</p>
+         </section>
+       </section>
+     </main>
+   );
 
 export default App;
