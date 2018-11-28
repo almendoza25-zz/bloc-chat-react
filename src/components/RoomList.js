74bdfa8d31f30212c9576a66b4646d31f09d2310
Alfredo Mendoza <almendoza25@gmail.com>
2018-11-28 09:31:24 -0800

    Checkpoint 3 completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
+import './RoomList.css';
+import Modal from 'react-awesome-modal';
 
 class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      visible: false,
+      roomName: '',
     };
-  this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+    this.roomsRef = this.props.firebase.database().ref('rooms');
 }
 
 componentDidMount() {
@@ -17,14 +23,76 @@ componentDidMount() {
   });
 }
 
+openModal() {
+   this.setState({visible: true});
+ }
+
+ closeModal() {
+   this.setState({visible: false});
+ }
+
+ handleChange(event) {
+   this.setState({roomName: event.target.value});
+ }
+
+ createRoom(event) {
+   this.roomsRef.push({name: this.state.roomName});
+   this.setState({roomName: ''});
+   event.preventDefault();
+ }
+
 render() {
   return (
-    <section className="rooms">
-      {this.state.rooms.map(room => (
-        <p key={room.key}>{room.name}</p>
-      ))}
-    </section>
+    <div>
+         <section className="rooms">
+           {this.state.rooms.map(room => (
+             <p key={room.key}>{room.name}</p>
+           ))}
+         </section>
+
+         <section>
+           <input
+             className="add-room-button"
+             type="button"
+             value="Create New Room"
+             onClick={() => this.openModal()}
+           />
+         </section>
+
+         <Modal
+           className="add-room-popup"
+           visible={this.state.visible}
+           width="400"
+           height="300"
+           effect="fadeInUp"
+           onClickAway={() => this.closeModal()}
+         >
+           <form
+             className="add-room-form"
+             onSubmit={() => {
+               this.createRoom();
+             }}
+           >
+             <label>
+               Enter New Room Name:
+               <input
+                 className="form-field"
+                 type="text"
+                 placeholder="New Room Name"
+                 value={this.state.roomName}
+                 onChange={this.handleChange}
+               />
+             </label>
+             <input className="submit-button" type="submit" />
+           </form>
+
+           <button className="close-modal-button" type="button" onClick={() => this.closeModal()}>
+             Close
+           </button>
+         </Modal>
+       </div>
   );
 }
 }
+
 export default RoomList;
